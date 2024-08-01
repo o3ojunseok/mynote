e1f0b8733abdb506a648f8f69748e3a3c6ab5831
o3ojunseok <o3ojunseok@gmail.com>
2024-08-01 21:20:20 +0900

    Feat: MyNoteRequest

---
+++ src/main/java/com/note/request/MyNoteRequest.java
@@ -0,0 +1,25 @@
+package com.note.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+//@Setter
+@Builder
+@AllArgsConstructor
+@ToString
+public class MyNoteRequest {
+    @Schema(description = "제목", example = "마이노트 제목입니다.", required = true)
+    @NotBlank
+    private String title;
+
+    @Schema(description = "내용", example = "마이노트 내용입니다.", required = true)
+    @NotBlank
+    private String content;
+
+    @Schema(description = "완료 여부", example = "true")
+    private boolean done;
+
+    @Schema(description = "태그 리스트", example = "[ {\"deleted\" : \"false\", \"tagName\" : \"Kubernetes\"}, {\"deleted\" : \"false\", \"tagName\" : \"Docker\"} ]")
+    private List<TagRequest> tag;
+}
