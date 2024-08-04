6304f8b5803313557c115d7fa02eef31fe44be8b
o3ojunseok <o3ojunseok@gmail.com>
2024-08-04 17:20:41 +0900

    Feat: save mynote controller

--- src/main/java/com/note/mynote/MyNoteController.java
+++ src/main/java/com/note/mynote/MyNoteController.java
@@ -1,9 +1,14 @@
 package com.note.mynote;
 
 import com.note.mynote.entity.MyNoteEntity;
+import com.note.mynote.request.MyNoteRequest;
 import com.note.mynote.service.MyNoteService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,4 +27,11 @@ public class MyNoteController {
         log.info("get all");
         return myNoteService.findAll();
     }
+
+    @PostMapping("/mynote")
+    ResponseEntity<MyNoteRequest> addNoteAndTag(@RequestBody MyNoteRequest noteRequest) {
+        MyNoteRequest save = myNoteService.save(noteRequest);
+        return new ResponseEntity<>(save, HttpStatus.CREATED);
+    }
+
 }
